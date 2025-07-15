256fde152941512fcf2718c543c45456806e5789
JoArDiTo <sm.armando05@gmail.com>
2025-07-15 17:47:04 -0500

    feat: Implement Commitment Letters table with filtering and pagination

---
+++ src/components/tables/commitment_letters/CommitmentLettersTable.jsx
@@ -0,0 +1,209 @@
+import PropTypes from 'prop-types';
+import { memo, useState } from 'react';
+import { Badge, Box, Group, HStack, Table } from '@chakra-ui/react';
+import { Pagination } from '@/components/ui';
+import { usePaginationSettings } from '@/components/navigation/usePaginationSettings';
+import { SortableHeader } from '@/components/ui/SortableHeader';
+import { format, parseISO } from 'date-fns';
+import SkeletonTable from '@/components/ui/SkeletonTable';
+import useSortedData from '@/utils/useSortedData';
+import { usePaginatedInfiniteData } from '@/components/navigation';
+
+const Row = memo(
+	({ item, startIndex, index, refetch, permissions, sortConfig, data }) => {
+		const statusDisplay = [
+			{ id: 1, label: 'Pendiente', bg: '#AEAEAE', color: '#F5F5F5' },
+			{ id: 2, label: 'Generado', bg: '#C6E7FC', color: '#0661D8' },
+			{ id: 3, label: 'Verificado', bg: '#D0EDD0', color: '#2D9F2D' },
+			{ id: 4, label: 'Expirado', bg: '#F7CDCE', color: '#E0383B' },
+		];
+
+		return (
+			<Table.Row key={item.id} bg={{ base: 'white', _dark: 'its.gray.500' }}>
+				<Table.Cell>
+					{sortConfig?.key === 'index' && sortConfig?.direction === 'desc'
+						? data.length - (startIndex + index)
+						: startIndex + index + 1}
+				</Table.Cell>
+				<Table.Cell>{format(parseISO(item.request_date), 'dd/MM/yyyy')}</Table.Cell>
+				<Table.Cell>{item.program_name}</Table.Cell>
+				<Table.Cell>{item.document_num}</Table.Cell>
+				<Table.Cell>{item.applicant_name}</Table.Cell>
+				<Table.Cell>
+					<Badge
+						bg={statusDisplay.find((status) => status.id === item.status)?.bg}
+						color={
+							statusDisplay.find((status) => status.id === item.status)?.color
+						}
+					>
+						{statusDisplay.find((status) => status.id === item.status)?.label ||
+							'N/A'}
+					</Badge>
+				</Table.Cell>
+				<Table.Cell>
+					<HStack justify='space-between'>
+						<Group gap={1}>
+              
+						</Group>
+					</HStack>
+				</Table.Cell>
+			</Table.Row>
+		);
+	}
+);
+
+Row.displayName = 'Row';
+
+Row.propTypes = {
+	item: PropTypes.object,
+	refetch: PropTypes.func,
+	permissions: PropTypes.array,
+	startIndex: PropTypes.number,
+	index: PropTypes.number,
+	sortConfig: PropTypes.object,
+	data: PropTypes.array,
+};
+
+export const CommitmentLettersTable = ({
+	isLoading,
+	data,
+	refetch,
+	permissions,
+	fetchNextPage,
+	hasNextPage,
+	isFetchingNextPage,
+}) => {
+	const { pageSize, setPageSize, pageSizeOptions } = usePaginationSettings();
+	const [sortConfig, setSortConfig] = useState(null);
+
+	const sortedData = useSortedData(data, sortConfig);
+	const {
+		currentPage,
+		startIndex,
+		visibleRows,
+		loadUntilPage,
+		setCurrentPage,
+	} = usePaginatedInfiniteData({
+		data: sortedData,
+		pageSize,
+		fetchNextPage,
+		hasNextPage,
+		isFetchingNextPage,
+	});
+
+	return (
+		<Box
+			bg={{ base: 'white', _dark: 'its.gray.500' }}
+			p='3'
+			borderRadius='10px'
+			overflow='hidden'
+			boxShadow='md'
+		>
+			<Table.ScrollArea>
+				<Table.Root size='sm' w='full' striped>
+					<Table.Header>
+						<Table.Row bg={{ base: 'its.100', _dark: 'its.gray.400' }}>
+							<Table.ColumnHeader alignContent={'start'}>
+								<SortableHeader
+									label='N°'
+									columnKey='index'
+									sortConfig={sortConfig}
+									onSort={setSortConfig}
+								/>
+							</Table.ColumnHeader>
+							<Table.ColumnHeader alignContent={'start'}>
+								<SortableHeader
+									label='Fecha de vencimiento'
+									columnKey='due_date'
+									sortConfig={sortConfig}
+									onSort={setSortConfig}
+								/>
+							</Table.ColumnHeader>
+							<Table.ColumnHeader alignContent={'start'}>
+								<SortableHeader
+									label='Id de Orden'
+									columnKey='id_orden'
+									sortConfig={sortConfig}
+									onSort={setSortConfig}
+								/>
+							</Table.ColumnHeader>
+							<Table.ColumnHeader alignContent={'start'}>
+								<SortableHeader
+									label='N° Documento'
+									columnKey='document_num'
+									sortConfig={sortConfig}
+									onSort={setSortConfig}
+								/>
+							</Table.ColumnHeader>
+							<Table.ColumnHeader alignContent={'start'}>
+								<SortableHeader
+									label='Correo'
+									columnKey='email'
+									sortConfig={sortConfig}
+									onSort={setSortConfig}
+								/>
+							</Table.ColumnHeader>
+							<Table.ColumnHeader alignContent={'start'}>
+								<SortableHeader
+									label='Estado'
+									columnKey='status_display'
+									sortConfig={sortConfig}
+									onSort={setSortConfig}
+								/>
+							</Table.ColumnHeader>
+							<Table.ColumnHeader alignContent={'start'}>
+								Acciones
+							</Table.ColumnHeader>
+						</Table.Row>
+					</Table.Header>
+					<Table.Body>
+						{isLoading ? (
+							<SkeletonTable columns={8} />
+						) : visibleRows?.length > 0 ? (
+							visibleRows?.map((item, index) => (
+								<Row
+									key={item.id}
+									item={item}
+									refetch={refetch}
+									startIndex={startIndex}
+									index={index}
+									permissions={permissions}
+									sortConfig={sortConfig}
+									data={data}
+								/>
+							))
+						) : (
+							<Table.Row>
+								<Table.Cell colSpan={8} textAlign='center' py={2}>
+									No hay datos disponibles.
+								</Table.Cell>
+							</Table.Row>
+						)}
+					</Table.Body>
+				</Table.Root>
+			</Table.ScrollArea>
+
+			<Pagination
+				count={data?.length}
+				pageSize={pageSize}
+				currentPage={currentPage}
+				pageSizeOptions={pageSizeOptions}
+				onPageChange={loadUntilPage}
+				onPageSizeChange={(size) => {
+					setPageSize(size);
+					setCurrentPage(1);
+				}}
+			/>
+		</Box>
+	);
+};
+
+CommitmentLettersTable.propTypes = {
+	isLoading: PropTypes.bool,
+	data: PropTypes.array,
+	refetch: PropTypes.func,
+	permissions: PropTypes.array,
+  fetchNextPage: PropTypes.func,
+  hasNextPage: PropTypes.bool,
+  isFetchingNextPage: PropTypes.bool,
+};
--- src/views/admin/debt_requests/CommitmentLetters.jsx
+++ src/views/admin/debt_requests/CommitmentLetters.jsx
@@ -1,9 +1,188 @@
+import { ReactSelect } from "@/components";
+import { CommitmentLettersTable } from "@/components/tables/commitment_letters";
+import { Field } from "@/components/ui";
+import { useReadPrograms } from "@/hooks";
+import { useReadUserLogged } from "@/hooks/users/useReadUserLogged";
+import { Button, Card, Flex, Heading, Icon, Input, SimpleGrid, Stack } from "@chakra-ui/react";
+import { useState } from "react";
+import { FiFileText, FiTrash } from "react-icons/fi";
+
 export const CommitmentLetters = () => {
+  const { data: profile } = useReadUserLogged();
+  const roles = profile?.roles || [];
+  const permissions = roles
+    .flatMap((r) => r.permissions || [])
+    .map((p) => p.guard_name);
+  
+  /*
+  const {
+    data: dataFractionationRequests,
+    refetch: fetchFractionationRequests,
+    isLoading: loadingFractionationRequests,
+    fetchNextPage: fetchNextPageFractionationRequests,
+		hasNextPage: hasNextPageFractionationRequests,
+		isFetchingNextPage: isFetchingNextPageFractionationRequests,
+  } = useReadDebtRequests();
+  */
+  const dataFractionationRequests = {
+     results: [
+     {
+       id: 1,
+       request_date: '2023-10-01',
+       status: 1,
+       amount: 1000,
+       program: 1,
+       program_name: 'Program A',
+       applicant_name: 'John Doe',
+       document_num: 'DOC12345',
+       request_path: '/path/to/request/1'
+     },
+     {
+       id: 2,
+       request_date: '2023-10-02',
+       status: 1,
+       amount: 2000,
+       program: 2,
+       program_name: 'Program B',
+       applicant_name: 'Jane Smith',
+       document_num: 'DOC67890',
+       request_path: '/path/to/request/2'
+     },
+     {
+       id: 3,
+       request_date: '2023-10-03',
+       status: 1,
+       amount: 1500,
+       program: 1,
+       program_name: 'Program A',
+       applicant_name: 'Alice Johnson',
+       document_num: 'DOC54321',
+       request_path: '/path/to/request/3'
+     }
+    ]
+  };
+
+  const {
+    data: dataPrograms,
+    isLoading: loadingPrograms,
+  } = useReadPrograms();
+
+  const ProgramOptions = dataPrograms?.results?.map((program) => ({
+    value: program.id,
+    label: program.name,
+  })) || [];
+
+  const StatusOptions = [
+		{ value: 1, label: 'Pendiente' },
+		{ value: 2, label: 'Validado' },
+		{ value: 3, label: 'Expirado' },
+	];
+
+  const [selectedProgram, setSelectedProgram] = useState(null);
+  const [selectedStatus, setSelectedStatus] = useState(null);
+  const [applicantName, setApplicantName] = useState('');
+
+  const hasActiveFilters = selectedProgram || selectedStatus || applicantName;
+
+  const clearFilters = () => {
+		setSelectedProgram(null);
+		setSelectedStatus(null);
+		setApplicantName('');
+	};
+
+  const filteredRequests = dataFractionationRequests?.results?.filter((request) => {
+    const matchProgram = selectedProgram ? request.program === selectedProgram.value : true;
+    const matchStatus = selectedStatus ? request.status === selectedStatus.value : true;
+    const matchApplicantName = applicantName
+      ? request.applicant_name.toLowerCase().includes(applicantName.toLowerCase())
+      : true;
+    return matchProgram && matchStatus && matchApplicantName;
+  });
+
+
   return (
-    <div>
-      <h1>Commitment Letters</h1>
-      <p>This is the Commitment Letters page.</p>
-      {/* Add your content here */}
-    </div>
+    <Stack gap={4}>
+      <Card.Root>
+        <Card.Header>
+          <Flex
+            justify='space-between'
+            align='center'
+            direction={{ base: 'column', sm: 'row' }}
+            gap={4} // Espacio entre filas en pantallas pequeñas
+          >
+            {/* Izquierda */}
+            <Flex align='center' gap={2}>
+              <Icon as={FiFileText} boxSize={5} color='blue.600' />
+              <Heading fontSize='24px'>Solicitudes de Fraccionamiento</Heading>
+            </Flex>
+
+            {/* Derecha */}
+            <Stack direction='row' spacing={2} align='center'>
+              {hasActiveFilters && (
+                <Button
+                  variant='outline'
+                  colorPalette='red'
+                  size='sm'
+                  onClick={clearFilters}
+                >
+                  <FiTrash />
+                  Limpiar Filtros
+                </Button>
+              )}
+            </Stack>
+          </Flex>
+        </Card.Header>
+
+        <Card.Body>
+          <Stack gap={4} mb={4}>
+            <SimpleGrid columns={{ base: 1, sm: 3 }} gap={6}>
+              <Field label='Programa:'>
+                <ReactSelect
+                  placeholder='Seleccionar programa'
+                  value={selectedProgram}
+                  onChange={setSelectedProgram}
+                  variant='flushed'
+                  size='xs'
+                  isSearchable
+                  isClearable
+                  options={ProgramOptions}
+                  isLoading={loadingPrograms}
+                />
+              </Field>
+              <Field label='Estado:'>
+                <ReactSelect
+                  placeholder='Seleccionar'
+                  value={selectedStatus}
+                  onChange={(selected) => setSelectedStatus(selected)}
+                  variant='flushed'
+                  size='xs'
+                  isSearchable
+                  isClearable
+                  options={StatusOptions}
+                />
+              </Field>
+              <Field label='Nombre del postulante:'>
+                <Input
+                  placeholder='Buscar por nombre del postulante'
+                  value={applicantName}
+                  onChange={(e) => setApplicantName(e.target.value)}
+                />
+              </Field>
+            </SimpleGrid>
+          </Stack>
+        </Card.Body>
+      </Card.Root>
+      
+      <CommitmentLettersTable
+        isLoading={false}
+	      data={filteredRequests}
+        refetch={() => {}}
+        permissions={permissions}
+        fetchNextPage={() => {}}
+        hasNextPage={false}
+        isFetchingNextPage={false}
+      />
+
+    </Stack>
   )
 }
