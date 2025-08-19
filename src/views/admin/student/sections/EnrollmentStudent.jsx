cb0b6a0523fc352a221fae488e52c7df2afd18ae
Rodrigo Torres <torres.rodrigo.752@gmail.com>
2025-08-18 23:59:20 -0500

    Add enrollment creation modal and hook
    Introduces AddEnrollmentStudentForm modal for creating student enrollments and a useCreateEnrollments hook. Updates EnrollmentStudent section to support filtering by program and adding new enrollments. Enhances enrollment management in the admin student view.

--- src/views/admin/student/sections/EnrollmentStudent.jsx
+++ src/views/admin/student/sections/EnrollmentStudent.jsx
@@ -1,28 +1,91 @@
 import { StudentTuitionTable } from '@/components/tables/students/StudentTuitionTable';
 
-import { Card, Heading, HStack, Icon, SimpleGrid } from '@chakra-ui/react';
+import {
+	Card,
+	Heading,
+	HStack,
+	Icon,
+	SimpleGrid,
+	Stack,
+} from '@chakra-ui/react';
 import { FiClipboard } from 'react-icons/fi';
 import PropTypes from 'prop-types';
+import { AddEnrollmentStudentForm } from '../modals/AddEnrollmentStudentForm';
+import { ReactSelect } from '@/components';
+import { useEffect, useMemo, useState } from 'react';
 
 export const EnrollmentStudent = ({
 	dataStudent,
 	myEnrollment,
 	isLoadingEnrollment,
 }) => {
+	const [selectProgram, setSelectProgram] = useState(null);
+	console.log(myEnrollment, selectProgram);
+	const ProgramsOptions = useMemo(
+		() =>
+			dataStudent?.admission_programs?.map((program) => ({
+				label: program.program_name,
+				value: program.program,
+			})) || [],
+		[dataStudent]
+	);
+
+	useEffect(() => {
+		if (ProgramsOptions.length > 0 && !selectProgram) {
+			setSelectProgram(ProgramsOptions[0]);
+		}
+	}, [ProgramsOptions, selectProgram]);
+
+	const filteredEnrollment = useMemo(() => {
+		if (!myEnrollment) return [];
+		if (!selectProgram) return myEnrollment; // si no hay selección, muestra todo
+
+		return Array.isArray(myEnrollment)
+			? myEnrollment.filter(
+					(enroll) => enroll.program_name === selectProgram.label
+				)
+			: myEnrollment.program_name === selectProgram.label
+				? [myEnrollment]
+				: [];
+	}, [myEnrollment, selectProgram]);
+
 	return (
 		<SimpleGrid columns={{ base: 1, lg: 1 }} gap={6}>
 			<Card.Root shadow={'md'}>
 				<Card.Header pb={0}>
-					<HStack gap={2}>
-						<Icon as={FiClipboard} boxSize={5} />
-						<Heading size='md'>Matrículas</Heading>
-					</HStack>
+					<Stack
+						justify={'space-between'}
+						align='center'
+						direction={{ base: 'column', md: 'row' }}
+					>
+						<HStack>
+							<Icon as={FiClipboard} boxSize={5} />
+							<Heading size='md'>Matrículas</Heading>
+						</HStack>
+
+						<HStack>
+							<ReactSelect
+								placeholder='Filtrar por programa...'
+								value={selectProgram}
+								onChange={(value) => setSelectProgram(value)}
+								variant='flushed'
+								size='xs'
+								isSearchable
+								isClearable
+								options={ProgramsOptions}
+							/>
+							<AddEnrollmentStudentForm
+								dataStudent={dataStudent}
+								selectProgram={selectProgram}
+							/>
+						</HStack>
+					</Stack>
 				</Card.Header>
 
 				<Card.Body>
 					<StudentTuitionTable
 						dataStudent={dataStudent}
-						data={myEnrollment}
+						data={filteredEnrollment}
 						isLoading={isLoadingEnrollment}
 					/>
 				</Card.Body>
