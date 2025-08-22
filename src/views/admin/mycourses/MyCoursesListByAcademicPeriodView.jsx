a642920558b01eb0afd3a330c8aeaad718530f5b
JoArDiTo <sm.armando05@gmail.com>
2025-08-21 19:15:08 -0500

    refactor: streamline program options and enhance grades record section with caching

--- src/views/admin/mycourses/MyCoursesListByAcademicPeriodView.jsx
+++ src/views/admin/mycourses/MyCoursesListByAcademicPeriodView.jsx
@@ -15,7 +15,6 @@ import {
 	Tabs,
 } from '@chakra-ui/react';
 import { useNavigate } from 'react-router';
-import { useReadMyPrograms } from '@/hooks/person/useReadMyPrograms';
 import { useState, useEffect } from 'react';
 import { ReactSelect } from '@/components';
 import {
@@ -28,12 +27,10 @@ import { GenerateAcademicTranscriptPdfModal } from '@/components/modals/mycourse
 export const MyCoursesListByAcademicPeriodView = () => {
 	const { data: dataUser, isLoading } = useReadUserLogged();
 	const studentUUID = dataUser?.student?.uuid;
+  console.log('User Data:', dataUser?.student?.admission_programs);
 
-	const { data: dataMyPrograms, isLoading: isLoadingMyPrograms } =
-		useReadMyPrograms();
-
-	const MyProgramsOptions = dataMyPrograms?.map((program) => ({
-		value: program.program_id,
+	const MyProgramsOptions = dataUser?.student?.admission_programs?.map((program) => ({
+		value: program.program,
 		label: program.program_name,
 	}));
 
@@ -42,7 +39,7 @@ export const MyCoursesListByAcademicPeriodView = () => {
 
 	useEffect(() => {
 		if (MyProgramsOptions && !programEnrolled) {
-			setProgramEnrolled(MyProgramsOptions[0]);
+			setProgramEnrolled(MyProgramsOptions[MyProgramsOptions.length - 1]);
 		}
 	}, [MyProgramsOptions, programEnrolled]);
 
@@ -104,26 +101,12 @@ export const MyCoursesListByAcademicPeriodView = () => {
 							fontSize='sm'
 						>
 							<ReactSelect
-								isLoading={isLoadingMyPrograms}
+								isLoading={isLoading}
 								options={MyProgramsOptions}
 								value={programEnrolled}
 								onChange={setProgramEnrolled}
 							/>
 						</Box>
-					</Flex>
-					<Flex
-						w='fit'
-						bg='purple.200'
-						py={1}
-						px={4}
-						borderRadius='md'
-						alignItems='center'
-						fontSize='md'
-					>
-						<Text fontWeight='bold' color='purple.700'>
-							Periodo Académico:
-						</Text>
-						<Text pl={4}>Ciclo {'string'}</Text>
 					</Flex>
 				</Flex>
 				<Flex
@@ -168,7 +151,7 @@ export const MyCoursesListByAcademicPeriodView = () => {
 				variant='plain'
 				my={3}
 			>
-				<Flex justify='space-between' align='center'>
+				<Flex overflow='hidden' direction={{ base: 'column', md: 'row' }} justify='space-between' align={{ base: 'flex-start', md: 'center' }} gap={2}>
 					<Tabs.List bg='blue.100' rounded='l3' p='1'>
 						<Tabs.Trigger color='blue.600' fontSize={'sm'} value='courses'>
 							Cursos
